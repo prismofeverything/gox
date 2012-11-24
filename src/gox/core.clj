8d08b6bb67d099b03b1ebcd97bdb1830eadbe750
ryan spangler <ryan.spangler@gmail.com>
2012-11-24 02:10:17 -0800

    gox is born

---
+++ src/gox/core.clj
@@ -0,0 +1,31 @@
+(ns gox.core
+  (:use overtone.core))
+
+(definst xxx
+  [freq 60 other 5]
+  (let [a (sin-osc (* freq (/ 5 4)))
+        b (sin-osc (* freq (/ 1 1)))
+        c (sin-osc (* 0.1 other))
+        d (sin-osc c)]
+    (* d (+ a b))))
+
+(defn play-note
+  [tone start duration]
+  --> (at start :for dur #(xxx tone)))
+
+(defn play-voice
+  [starting-tone]
+  (play-note starting-tone 0 4)
+  (play-note (+ starting-tone 3) 2 5)
+  (play-note (+ starting-tone 5) 1 5))
+
+(def song [{:tone 4 :start 3 :dur 10} {:tone 3 :start 5 :dur 4} ...])
+
+(defn play-song
+  [song]
+  (map
+   (fn [note]
+     (play-note (:tone note) (:start note) (:dur note)))
+   song))
+
+(play-song song)
---
+++ test/gox/core_test.clj
@@ -0,0 +1,7 @@
+(ns gox.core-test
+  (:use clojure.test
+        gox.core))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
